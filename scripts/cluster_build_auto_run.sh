0fa6efa9170e1805411f8571b60224c555fc204a
Alston Knight <alkn409@gmail.com>
2026-04-03 09:24:31 -0400

    Fixed pathing to other scripts

--- scripts/cluster_build_auto_run.sh
+++ scripts/cluster_build_auto_run.sh
@@ -2,11 +2,7 @@
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 
 "$SCRIPT_DIR/cluster_build.sh"
-
 source "$SCRIPT_DIR/env.sh"
-
-cd "$PROJECT_ROOT/cluster_build"
-./tests/test_flac_prelim.sh
+"$SCRIPT_DIR/test_flac_prelim.sh"
